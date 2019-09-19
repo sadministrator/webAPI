69feee1517097667cf4e9b224f36eb6cd254e273
Marco Serrano <marcoTheWebDev@gmail.com>
2019-09-19 11:59:44 -0500

    Added PostModel and CommentModel

---
+++ WebApplication8/Models/CommentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public PostModel Post { get; set; }
+
+        public UserModel User { get; set; }
+    }
+}
---
+++ WebApplication8/Models/PostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Models
+{
+    public class PostModel
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public UserModel User { get; set; }
+    }
+}
